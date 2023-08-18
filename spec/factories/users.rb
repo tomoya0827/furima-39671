e4042aa8e64841412e662dbf83c0a93085c92868
tomoya0827 <140814716+tomoya0827@users.noreply.github.com>
2023-08-18 17:13:49 +0900

    テストコードの記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,16 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'test'}
-    email                 {'test@example'}
-    password              {'a00000'}
+    transient do
+      person { Gimei.name }
+    end
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.email}
+    password              { "a1" + Faker::Internet.password(min_length: 6) }
     password_confirmation {password}
-    first_name            {'test'}
-    family_name           {'test'}
-    first_name_kana       {'test'}
-    family_name_kana      {'test'}
-    birth_day             {'test'}
+    first_name            { person.first.kanji }
+    family_name           { person.family.kanji }
+    first_name_kana       { person.first.katakana }
+    family_name_kana      { person.family.katakana }
+    birth_day             { Faker::Date.backward }
   end
 end
