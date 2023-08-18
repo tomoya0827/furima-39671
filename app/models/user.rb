2bb7a1b5896a0298fa42c01e717f052b84c2df3b
tomoya0827 <140814716+tomoya0827@users.noreply.github.com>
2023-08-18 18:16:28 +0900

    訂正箇所の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
     validates :family_name
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: '全角カナを使用してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カナを使用してください' } do
     validates :first_name_kana
     validates :family_name_kana
   end
