df0a9feeac4828713846e9a84a6159d1f951c266
tomoya0827 <140814716+tomoya0827@users.noreply.github.com>
2023-08-19 16:18:40 +0900

    ActiveHashの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ApplicationRecord
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :cost
+  belongs_to :prefecture
+  belongs_to :delivery_day
 end
