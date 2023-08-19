f3051a93067642aa8f4fe3ba6fda6fb87230d9b9
tomoya0827 <140814716+tomoya0827@users.noreply.github.com>
2023-08-19 17:18:08 +0900

    バリデーションの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,16 @@ class Product < ApplicationRecord
   belongs_to :cost
   belongs_to :prefecture
   belongs_to :delivery_day
+
+  validates :image, presence: true
+  validates :product_name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :cost_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+
 end
