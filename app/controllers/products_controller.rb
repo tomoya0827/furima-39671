f0f10d7c1ef54955c5e03211241024abdf56a19e
tomoya0827 <140814716+tomoya0827@users.noreply.github.com>
2023-08-21 17:20:45 +0900

    エラーメッセージ表示の設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,7 +13,7 @@ class ProductsController < ApplicationController
     if @product.save
       redirect_to root_path
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -14,7 +14,7 @@ class Product < ApplicationRecord
   validates :description, presence: true
   validates :price, presence: true,
              numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-             format: { with: /\A[0-9]+\z/, message: "半角数字を入力してください" }
+             format: { with: /\A[0-9]+\z/ }
   
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
