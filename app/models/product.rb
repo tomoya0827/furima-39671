ad5a87d3d9ba8fd3f7fdc8f8c9aa22cd36f19a1c
tomoya0827 <140814716+tomoya0827@users.noreply.github.com>
2023-08-21 16:45:32 +0900

    価格のバリデーション変更

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,7 +12,9 @@ class Product < ApplicationRecord
   validates :image, presence: true
   validates :product_name, presence: true
   validates :description, presence: true
-  validates :price, presence: true
+  validates :price, presence: true,
+             numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+             format: { with: /\A[0-9]+\z/, message: "半角数字を入力してください" }
   
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
