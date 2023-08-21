97a1ede82f30dd6dae5ba0bfec8cb5a035525fb5
tomoya0827 <140814716+tomoya0827@users.noreply.github.com>
2023-08-21 12:08:33 +0900

    createアクションの定義

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,15 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+
+  @product = Product.new(product_params)
+  if @product.save
+    redirect_to root_path
+  else
+    render :new
+
+  private
+  def product_params
+    params.require(:product).permit(:image, :product_name, :description, :category_id, :condition_id, :cost_id, :prefecture_id, :delivery_day_id, :price)
+  end
 end
