9a4ffdfa99bff5fabaed50aba8f71fd216aa0fcf
tomoya0827 <140814716+tomoya0827@users.noreply.github.com>
2023-08-21 11:17:28 +0900

    商品出品ページ表示の設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,10 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
+  end
+
+  def new
+    @product = Product.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'products/index'
   root to: "products#index"
+  resources :products, only: [:new]
 end
