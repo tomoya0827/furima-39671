ec440cdbae2f22eab3c24075efe7abd8bff2c0ce
tomoya0827 <140814716+tomoya0827@users.noreply.github.com>
2023-08-23 10:52:17 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'products/index'
   root to: "products#index"
-  resources :products, only: [:index, :new, :create, :show]
+  resources :products, only: [:index, :new, :create, :show, :edit]
 end
