86697d154500ff3e4faad2f83974e6e450e3cbe7
tomoya0827 <140814716+tomoya0827@users.noreply.github.com>
2023-08-23 15:37:03 +0900

    beforeアクションでまとめる

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :set_product, only: [:edit, :show, :update]
+
 
   def index
     @products = Product.all.order("created_at DESC")
@@ -19,11 +21,9 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def edit
-    @product = Product.find(params[:id])
     if current_user.nil?
       redirect_to new_user_session_path
     elsif @product.user != current_user
@@ -32,7 +32,6 @@ class ProductsController < ApplicationController
   end
 
   def update
-    @product = Product.find(params[:id])
     if @product.update(product_params)
        redirect_to root_path
     else 
@@ -43,5 +42,9 @@ class ProductsController < ApplicationController
   private
   def product_params
     params.require(:product).permit(:image, :product_name, :description, :category_id, :condition_id, :cost_id, :prefecture_id, :delivery_day_id, :price)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
