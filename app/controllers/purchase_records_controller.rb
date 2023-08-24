2a63b3910b48bc40b43cc442d821c16022fbdc1d
tomoya0827 <140814716+tomoya0827@users.noreply.github.com>
2023-08-24 15:50:42 +0900

    コントローラーの設定

---
+++ app/controllers/purchase_records_controller.rb
@@ -0,0 +1,21 @@
+class PurchaseRecordsController < ApplicationController
+  def index
+    @purcha_address = PurchaAddress.new
+  end
+
+  def create
+    @purcha_address = PurchaAddress.new(purchase_record_params)
+    if @purcha_address.valid?
+      @purcha_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def purchase_record_params
+    params.require(:purcha_address).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_name, :purchase_record).merge(user_id: current_user.id)
+  end
+end
