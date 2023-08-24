abcaa0e1ea3bf2e0fca6488ceb20dffd5a963e6c
tomoya0827 <140814716+tomoya0827@users.noreply.github.com>
2023-08-24 17:18:26 +0900

    モデルの修正

--- app/models/purcha_address.rb
+++ app/models/purcha_address.rb
@@ -1,11 +1,11 @@
 class PurchaAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :city, :address, :building_name, :phone_name, :purchase_record, :product_id, :user_id
+  attr_accessor :post_code, :prefecture_id, :city, :address, :building_name, :phone_name, :product_id, :user_id
 
   def save
     purchase_record = PurchaseRecord.create(product_id: product_id, user_id: user_id)
 
     ShippingAddress.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address,building_name: building_name,
-      phone_name: phone_name, purchase_record: purchase_record, product_id: product_id)
+      phone_name: phone_name, purchase_record_id: purchase_record.id)
   end
 end
