fc93b4d8ff4fb82d1850517ce8a4189fb16a8c95
tomoya0827 <140814716+tomoya0827@users.noreply.github.com>
2023-08-24 17:40:50 +0900

    バリデーション追加

--- app/models/purcha_address.rb
+++ app/models/purcha_address.rb
@@ -2,6 +2,17 @@ class PurchaAddress
   include ActiveModel::Model
   attr_accessor :post_code, :prefecture_id, :city, :address, :building_name, :phone_name, :product_id, :user_id
 
+  with_options presence: true do
+    validates :product_id
+    validates :user_id
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city
+    validates :address
+    validates :phone_name, presence: true, length: { in: 10..11 }, numericality: { only_integer: true }
+    validates :purchase_record
+  end
+    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+
   def save
     purchase_record = PurchaseRecord.create(product_id: product_id, user_id: user_id)
 
