82ad18fd6962144196def68625891cc2d1f1fcf3
tomoya0827 <140814716+tomoya0827@users.noreply.github.com>
2023-08-25 17:36:22 +0900

    ログイン有無での遷移の設定

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,4 +1,7 @@
 class PurchaseRecordsController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+  before_action :check_seller, only: [:index]
+
   def index
     @product = Product.find(params[:product_id])
     @purcha_address = PurchaAddress.new
@@ -20,5 +23,12 @@ class PurchaseRecordsController < ApplicationController
 
   def purchase_record_params
     params.require(:purcha_address).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_name).merge(product_id: params[:product_id], user_id: current_user.id)
+  end
+
+  def check_seller
+    product = Product.find(params[:product_id])
+    if current_user == product.user
+      redirect_to root_path
+    end
   end
 end
